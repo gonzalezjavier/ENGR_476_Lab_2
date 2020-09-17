7f32a142f89313ff3127275144da4d28b95d5e01
Javier Gonzalez <gonzalezjavier925@gmail.com>
2020-09-17 13:02:45 -0700

    Initial Commit

---
+++ 478_Lab_2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  478_Lab_2
+//
+//  Created by Javier Gonzalez on 9/17/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
